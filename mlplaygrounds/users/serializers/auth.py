b17c3065d01753bb5684ed0ea9b48f3cdc30170a
= <=>
2020-05-16 22:00:02 -0400

    Registration Serializer & Login bug fixes

--- mlplaygrounds/users/serializers/auth.py
+++ mlplaygrounds/users/serializers/auth.py
@@ -3,16 +3,21 @@ from django.contrib.auth import authenticate, login, logout
 from rest_framework import serializers
 
 from mlplaygrounds.users.models import User
+from mlplaygrounds.users.validators.password import (
+    validate_is_alpha,
+    validate_is_numeric,
+    validate_not_short
+)
 
 
 class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=16)
-    password = serializers.CharField(max_length=255)
+    username = serializers.CharField(max_length=16, required=True)
+    password = serializers.CharField(max_length=255, required=True)
 
     def __init__(self, data=serializers.empty, **kwargs):
         super().__init__(None, data, **kwargs)
         
-        self.request = kwargs.get('request', None)
+        self.request = self.context.get('request', None)
         if self.request is None:
             raise serializers.ValidationError(
                 'Request needed to authenticate user.'
@@ -50,9 +55,9 @@ class LoginSerializer(serializers.Serializer):
 
 class LogoutSerializer(serializers.Serializer):
     def __init__(self, **kwargs):
-        super().init(instance=None, serializers.empty, **kwargs)
+        super().__init__(None, serializers.empty, **kwargs)
 
-        self.request = kwargs.get('request', None)
+        self.request = self.context.get('request', None)
         if self.request is None:
             raise serializers.ValidationError(
                 'Request needed to log out.'
@@ -60,3 +65,51 @@ class LogoutSerializer(serializers.Serializer):
 
     def save(self):
         return logout(self.request)
+
+
+class RegisterSerializer(serializers.Serializer):
+    first_name = serializers.CharField(max_length=255, required=True)
+    last_name = serializers.CharField(max_length=255, required=True)
+    registration_date = serializers.DateTimeField(read_only=True)
+    email = serializers.EmailField(max_length=255, required=True)
+    username = serializers.CharField(max_length=16, required=True)
+    password = serializers.CharField(max_length=255,
+                                     required=True,
+                                     validators=[
+                                         validate_is_alpha,
+                                         validate_is_numeric,
+                                         validate_not_short
+                                     ])
+    
+    def __init__(self, data=serializers.empty, **kwargs):
+        super().__init__(None, data, **kwargs)
+
+        self.request = self.context.get('request', None)
+        if self.request is None:
+            raise serializers.ValidationError(
+                'Request needed to log in user after registration.'
+            )
+        self.queryset = self._get_queryset()
+
+    def _get_queryset(self):
+        return User.objects
+
+    def validate_username(self, username):
+        if self.queryset.filter(username=username).exists():
+            raise serializers.ValidationError(
+                f'Username {username} already in use.'
+            )
+        return username
+
+    def validate_password(self, password):
+        return password
+
+    def validate_email(self, email):
+        if self.queryset.filter(email=email).exists():
+            raise serializers.ValidationError(
+                f'Email {email} already in use.'
+            )
+        return email
+
+    def create(self, validated_data):
+        return self.queryset.create_user(**validated_data)
--- mlplaygrounds/users/tests/test_auth_serializer.py
+++ mlplaygrounds/users/tests/test_auth_serializer.py
@@ -5,7 +5,7 @@ from rest_framework.serializers import ValidationError
 
 from django_mock_queries.query import MockSet, MockModel
 
-from mlplaygrounds.users.serializers.auth import LoginSerializer
+from mlplaygrounds.users.serializers.auth import LoginSerializer, RegisterSerializer
 
 from mlplaygrounds.users.models import User
 
@@ -24,7 +24,7 @@ class TestLoginSerializer(TestCase):
         serializer = LoginSerializer(data=data,
                                      context={'request': MagicMock()})
 
-        with patch.object(User, 'check_password', return_value=True) as mocked_method:
+        with patch.object(User, 'check_password', return_value=True):
             if serializer.is_valid():
                 user = serializer.save()
                 self.assertEqual(user.username, expected_user.username)
@@ -55,6 +55,52 @@ class TestLoginSerializer(TestCase):
         serializer = LoginSerializer(data=data,
                                      context={'request': MagicMock()})
         
-        with patch.object(User, 'check_password', return_value=False) as mocked_checker:
+        with patch.object(User, 'check_password', return_value=False):
             with self.assertRaises(ValidationError):
                 serializer.is_valid(raise_exception=True)
+
+
+class TestRegisterSerializer(TestCase):
+    def setUp(self):
+        self.users = MockSet(
+            MockModel(username='nottest', email='nottest@mail.com'),
+            MockModel(username='test', email='test@mail.com')
+        )
+        self.users.create_user = MagicMock(
+            return_value=MockModel(username='john'))
+
+        RegisterSerializer._get_queryset = MagicMock(return_value=self.users)
+    
+    def test_create(self):
+        data = {'username': 'john', 'password': 'Appleseed.123',
+                'email': 'john@mail.com', 'first_name': 'John',
+                'last_name': 'Appleseed'}
+
+        serializer = RegisterSerializer(data=data,
+                                        context={'request': MagicMock()})
+        
+        self.assertEqual(serializer.is_valid(raise_exception=True), True)
+        created_user = serializer.create(serializer.validated_data)
+        self.assertEqual(created_user.username, data['username'])
+
+    def test_invalid_user(self):
+        data = {'username': 'test', 'password': 'ValidPassword.123',
+                'email': 'anothertest@mail.com', 'first_name': 'Tester',
+                'last_name': 'Tested'}
+        
+        serializer = RegisterSerializer(data=data,
+                                        context={'request': MagicMock()})
+
+        with self.assertRaises(ValidationError):
+            serializer.is_valid(raise_exception=True)
+
+    def test_invalid_password(self):
+        data = {'username': 'validuser', 'password': 't123',
+                'email': 'anothertest@mail.com', 'first_name': 'Tester',
+                'last_name': 'Tested'}
+
+        serializer = RegisterSerializer(data=data,
+                                        context={'request': MagicMock()})
+
+        with self.assertRaises(ValidationError):
+            serializer.is_valid(raise_exception=True)
