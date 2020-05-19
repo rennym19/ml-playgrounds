dcb473d11580bf993432cefbb476d7e1f9531c59
Renny Montero <rennym19@gmail.com>
2020-05-19 16:36:48 -0400

    Added svg-loaders for babel and js-cookie

--- mlplaygrounds/frontend/webpack.config.js
+++ mlplaygrounds/frontend/webpack.config.js
@@ -29,6 +29,10 @@ module.exports = {
         test: /\.css$/i,
         exclude: /node_modules/,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.svg$/i,
+        use: ['svg-url-loader']
       }
     ]
   }
