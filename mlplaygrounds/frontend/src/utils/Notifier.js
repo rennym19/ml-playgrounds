7ee7f1f8283e7d98226ed5dc5f90c3381381b3e1
Renny Montero <rennym19@gmail.com>
2020-05-21 15:03:21 -0400

    Notifier utils

---
+++ mlplaygrounds/frontend/src/utils/Notifier.js
@@ -0,0 +1,34 @@
+import { store } from 'react-notifications-component'
+
+function notify(title, message, type) {
+  store.addNotification({
+    title: title,
+    message: message,
+    type: type,
+    insert: 'top',
+    container: 'top-center',
+    animationIn: ['animated', 'fadeIn'],
+    animationOut: ['animated', 'fadeOut'],
+    dismiss: {
+      duration: 5000,
+      showIcon: true
+    }
+  })
+}
+
+export function notifyErrors(errors) {
+  if (errors.hasOwnProperty('non_field_errors')) {
+    notifyNonFieldErrors(errors)
+  } else {
+    notifyFieldErrors(errors)
+  }
+}
+
+function notifyNonFieldErrors(errors) {
+  notify('Error', errors.non_field_errors[0], 'danger')
+}
+
+function notifyFieldErrors(errors) {
+  for (let field in errors)
+    notify(field, errors[field][0], 'danger')
+}
