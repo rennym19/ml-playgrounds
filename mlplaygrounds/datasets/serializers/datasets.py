f225cb1a14f85a3ac75bb61d646636210f0e3035
Renny Montero <rennym19@gmail.com>
2020-05-22 23:41:43 -0400

    Custom serialization for ObjectIds in DatasetSerializer

--- mlplaygrounds/datasets/serializers/datasets.py
+++ mlplaygrounds/datasets/serializers/datasets.py
@@ -1,16 +1,32 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import serializers
 
-from django.contrib.auth import get_user_model
+from bson.objectid import ObjectId
 
 from ..db.collections import Dataset
 
 
 class DatasetSerializer(serializers.Serializer):
+    uid = serializers.CharField(read_only=True)
     user_id = serializers.PrimaryKeyRelatedField(
         queryset=get_user_model().objects.all(), required=True
     )
     name = serializers.CharField(required=True)
     data = serializers.DictField(allow_empty=True, required=False)
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['uid'] = str(instance.uid)
+        return representation
+    
+    def to_internal_value(self, data):
+        internal_val = Dataset(**data)
+        
+        if not isinstance(data['uid'], ObjectId):
+            internal_val.uid = ObjectId(data['uid'])
+        
+        return internal_val
     
     def validate_name(self, name):
         if len(name) > 255:
